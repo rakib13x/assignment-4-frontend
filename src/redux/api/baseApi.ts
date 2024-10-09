66570a03e921de868f80bc5733a7a93327463dc0
rakib13x <rakib13x@gmail.com>
2024-10-10 00:34:21 +0600

    fixed some problem

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,19 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://assignment-4-lake-ten.vercel.app/api/v1",
+  credentials: "include",
+});
 // const baseQuery = fetchBaseQuery({
 //   // baseUrl: "https://assignment-4-lake-ten.vercel.app/api/v1",
 //   // baseUrl: "https://assignment-4-lake-ten.vercel.app/api/v1",
-//   baseUrl: "https://assignment-4-lake-ten.vercel.app/api/v1",
+//   baseUrl: "http://localhost:3000/api/v1",
 //   // baseUrl: "/api/v1",
 //   credentials: "include",
 // });
-const baseQuery = fetchBaseQuery({
-  // baseUrl: "https://assignment-4-lake-ten.vercel.app/api/v1",
-  // baseUrl: "https://assignment-4-lake-ten.vercel.app/api/v1",
-  baseUrl: "http://localhost:3000/api/v1",
-  // baseUrl: "/api/v1",
-  credentials: "include",
-});
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
