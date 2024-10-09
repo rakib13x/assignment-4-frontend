1c7f82ec669e6f264fe0d425b8a67f0d68ad7a09
rakib13x <rakib13x@gmail.com>
2024-10-10 01:49:34 +0600

    fixed cors issue

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl:
-    "https://assignment-4-cx2crmvrh-rakib13xgmailcoms-projects.vercel.app/api/v1",
+  baseUrl: "https://assignment-4-lake-ten.vercel.app/api/v1",
   credentials: "include",
 });
 
